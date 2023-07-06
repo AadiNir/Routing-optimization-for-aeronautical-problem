6f4850b5202ca3ffcb5123ea822807274dac3365
Aadinir <aadiniranjan27@gmail.com>
2023-07-06 15:58:06 +0530

    secondcommit

--- computernetwrksproject.py
+++ computernetwrksproject.py
@@ -28,19 +28,63 @@ def calculate_distance(alt1, lat1, lon1, alt2, lat2, lon2):
     distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
     return distance
 
-csv_filename='Dataset.csv'
+csv_filename='Computer-network/Dataset.csv'
 ls=read_csv_file(csv_filename)
 
 LHR=[81.73,0.4543,51.4700]
 EWR=[8.72,74.1745,40.6895]
 x=sys.maxsize
 i=0
+j=0
 ls3=[]
 
 def iterations(ls2,g):
     for q in range(2,5):
         ls2.append(ls[g][q])
     return ls2
+
+def calculate_delay(a,b,c,d,e,f):
+    a_rad = math.radians(a)
+    b_rad = math.radians(b)
+    delay1 = math.sin(a_rad)+math.cos(b_rad)+c
+    d_rad = math.radians(d)
+    e_rad = math.radians(e)
+    delay2 = math.sin(d_rad)+math.cos(e_rad)+f
+    
+    totaldelay = delay1+delay2
+
+
+
+    return totaldelay
+ls4=[]
+
+while(j<149):
+    n=j+1
+    ls1=ls[i][2:5]
+    a=float(ls1[0])
+    b=float(ls1[1])
+    c=float(ls1[2])
+    for k in range(n,149):
+        ls2=ls[n][2:5]
+        d=float(ls2[0])
+        e=float(ls2[1])
+        f=float(ls2[2])
+        a1=calculate_delay(a,b,c,d,e,f)
+        a2=calculate_delay(a,b,c,LHR[0],LHR[1],LHR[2])
+        a3=calculate_delay(a,b,c,EWR[0],EWR[1],EWR[2])
+        a4=calculate_delay(d,e,f,LHR[0],LHR[1],LHR[2])
+        a5=calculate_delay(d,e,f,EWR[0],EWR[1],EWR[2])
+        d1=a1+a2
+        d2=a1+a3
+        d3=a1+a4
+        d4=a1+a5
+        k=min(d1,d2,d3,d4)
+        if(k<x):
+            ls4.append(k)
+            x=k
+
+    x=sys.maxsize        
+    j+=1
 
 
 while(i<149):
@@ -53,9 +97,7 @@ while(i<149):
     c=float(ls1[2]) 
     
     for k in range(n,149):
-        # ls2=iterations(ls1,a)
         ls2 = ls[n][2:5]
-        # print(ls2)
         d=float(ls2[0])
         e=float(ls2[1])
         f=float(ls2[2])
@@ -64,12 +106,19 @@ while(i<149):
         a3=calculate_distance(a,b,c,EWR[0],EWR[1],EWR[2])
         a4=calculate_distance(d,e,f,LHR[0],LHR[1],LHR[2])
         a5=calculate_distance(d,e,f,EWR[0],EWR[1],EWR[2])
+  
+
         k=min(a1,a2,a3,a4,a5)
         if(k<x):
             ls3.append(k)
-
             x=k
+
+            
     x=sys.maxsize        
     i+=1
+minimum_data_latency=min(ls4)
+maximum_transmission_data=max(ls3)
 
-print(len(ls3))
+
+print(ls4.index(minimum_data_latency))
+print(ls3.index(maximum_transmission_data))
